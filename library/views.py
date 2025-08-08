92e9f2fbdbd985352342b251f43c868cd4b09914
Abodey <123SMD000@gmail.com>
2025-08-08 17:56:01 +0400

    done styling the login and signup pages

--- library/views.py
+++ library/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'library/index.html')
+
+# Authentication views
+def login_view(request):
+    if request.method == "POST":
+        return
+    else:
+        return render(request, 'library/login.html')
+    
+def signup_view(request):
+    if request.method == "POST":
+        return
+    else:
+        return render(request, 'library/signup.html')
