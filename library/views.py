b5671e87d251bbc22062901fc78eecf9535b59f9
Abodey <123SMD000@gmail.com>
2025-08-22 22:38:31 +0400

    designed the structure of the api

--- library/views.py
+++ library/views.py
@@ -2,15 +2,69 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
+import asyncio
+import httpx
+from urllib.parse import quote
+
 # Create your views here.
-
-def search(request):
-    return
-
 def index(request):
     return render(request, 'library/index.html')
+
+############## api ################
+# Gutenberg project
+async def gutenberg_fetch(title):
+    url = f"https://gutendex.com/books/?search={title}"
+    async with httpx.AsyncClient(timeout=20.0) as clinet:
+        r = await clinet.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            return [
+                {
+                    "title": book.get("title"),
+                    "authors": [author.get("name") for author in book.get("authors")] if book.get("authors") else ["Unknown"],
+                    "source": "Gutenberg Project",
+                    "url": f"https://gutenberg.org/ebooks/{book.get("id")}"
+                } for book in data["results"][:5]
+            ]
+    return []
+# Open Library
+async def openlibrary_fetch(title):
+    url = f"https://openlibrary.org/search.json?q={title}"
+    async with httpx.AsyncClient(timeout=20.0) as client:
+        r = await client.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            return [
+                {"source": "Open Library",
+                 "title": doc.get("title"),
+                 "authors": doc.get("author_name", ["Unknown"]),
+                 "url": f"archive.org/details/{doc.get('ia')[0]}" if doc.get("ia") else None
+                } for doc in data["docs"][:5]
+            ]
+    return []
+# Combine resources
+async def cobine_tasks(title):
+    gutenberg_task = gutenberg_fetch(title)
+    openlibrary_task = openlibrary_fetch(title)
+    results = await asyncio.gather(gutenberg_task, openlibrary_task)
+    return [book for fetch_result in results for book in fetch_result]
+
+# api endpoint
+def search(request):
+    title = request.GET.get("title")
+    if title:
+        encoded_title = quote(title)
+        results = asyncio.run(cobine_tasks(encoded_title))
+        return JsonResponse({"title": title, "results": results})
+    else:
+        return JsonResponse({"message": "Invalid book title", "status_code": 400})
+##########################################
+    
+    
+
+
 
 # Authentication views
 def login_view(request):
